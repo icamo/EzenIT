170d4d19947ff8f09c71d7ed61bb14b17be80229
ezen <ezen@DESKTOP-O03MT84>
2021-05-03 12:34:26 +0900

    20210503

---
+++ 20210503/src/package03/Ex11Test.java
@@ -0,0 +1,28 @@
+package package03;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ex11Test {
+
+	public static void main(String[] args) {
+		
+		Date now = new Date();
+		String strNow = now.toString();
+		System.out.println(strNow);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String strNow2 = sdf.format(now);
+		System.out.println(strNow2);
+		
+		sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		strNow2 = sdf.format(now);
+		System.out.println(strNow2);
+		
+		sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초 S");
+		strNow2 = sdf.format(now);
+		System.out.println(strNow2);
+		
+	}
+
+}
